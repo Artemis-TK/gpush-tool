779f71e28b62b1f1efb905d8e8257da68769b9c2
Lucas <alanderson.lucas@escolar.ifrn.edu.br>
2025-04-17 14:43:10 -0300

    Finalizando a ferramenta #1 commit

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SUCCESS_EXIT 0
+#define FAIL_EXIT 1
+
+int main(int argc, char const *argv[])
+{
+    if (argc < 2)
+    {
+        printf("O commit nao pode estar vazio. Digite algum valor para o commit.\nExemplo:\n\ngpush \"primeiro commit\"\n");
+        return FAIL_EXIT;
+    }
+
+    char mensagem[512] = {0};
+    for (int i = 1; i < argc; i++)
+    {
+        strcat(mensagem, argv[i]);
+        if (i < argc - 1)
+            strcat(mensagem, " ");
+    }
+
+    char comando[600];
+    snprintf(comando, sizeof(comando), "git commit -m \"%s\"", mensagem);
+
+    system("git pull origin main");
+    system("git add .");
+    system(comando);
+    system("git push origin main");
+
+    return SUCCESS_EXIT;
+}
